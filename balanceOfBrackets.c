0f6b4be419a7e8ed003f2b73d1972c3ad5e026f4
AleksandrVoskresenskii <aleksandrvoskresenskii06@gmail.com>
2024-09-29 01:12:23 +0300

    Add files via upload

---
+++ balanceOfBrackets.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool balanceOfBrackets(char brackets[]) {
+    //char brackets[] = "(45*(5+4) + 18)*13";
+
+    int balanceCount = 0, i = 0;
+
+    while (brackets[i] != '\0') {
+        if (brackets[i] == ')') {
+            balanceCount -= 1;
+        }
+        else if (brackets[i] == '(') {
+            balanceCount += 1;
+        }
+        if (balanceCount < 0) {
+            return 0;
+        }
+        i += 1;
+    }
+    if (balanceCount == 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+
+    return 0;
+}
+
+int main(void) {
+    char brackets[] = "(45*(5+4) + 18)*13";
+
+    if (balanceOfBrackets(brackets)) {
+        printf("true");
+    }
+    else if (!balanceOfBrackets(brackets)) {
+        printf("false");
+    }
+
+}
