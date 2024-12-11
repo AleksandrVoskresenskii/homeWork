57cab5556a37a6917bb594ed68c36c096c0c8cd0
AleksandrVoskresenskii <aleksandrvoskresenskii06@gmail.com>
2024-12-11 15:06:20 +0300

    New substringInString.c

--- substringInString.c
+++ substringInString.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-int countSubstring(char *S, char* S1) {
+int countSubstring(const char *S, const char *S1) {
     int count = 0;
-    int lenString = strlen(S);
-    int lenSubsting = strlen(S1);
+    int lenString = (int)strlen(S);
+    int lenSubsting = (int)strlen(S1);
+
+    // Если длина подстроки больше, чем длина строки, вхождений не будет
+    if (lenSubsting > lenString) {
+        return 0;
+    }
+
     for (int i = 0; i <= lenString - lenSubsting; ++i) {
-        int countLenString = 0;
-        for (int j = i; j < i + lenSubsting; ++j) {
-            if (S[j] == S1[j - i]) {
-                countLenString++;
+        int matched = 1; // флаг, что все символы совпали
+        for (int j = 0; j < lenSubsting; ++j) {
+            // Как только встретилось несоответствие, прерываем цикл
+            if (S[i+j] != S1[j]) {
+                matched = 0;
+                break;
             }
         }
-        if (countLenString == lenSubsting) {
+        if (matched) {
             count++;
         }
     }
-
 
     return count;
 }
 
 int main() {
-    char *S = "asdfjkl;asdfasdfasdf", *S1 = "asdf";
+    const char *S = "asdfjkl;asdfasdfasdf";
+    const char *S1 = "asdf";
     printf("S1 in S %d times\n", countSubstring(S, S1));
-
     return 0;
 }
